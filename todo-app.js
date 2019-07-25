6a4d74e578c1cc784d2039eb9b9958a535617961
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-07-25 12:22:25 +0200

    adding a function that avoid add a todo with start with empty space or empty string

--- todo-app.js
+++ todo-app.js
@@ -1,8 +1,6 @@
 'use strict';
 const todos = getSavedTodos()
 
-
-
 const filters = {
     searchText: '',
     hideCompleted: false
@@ -15,15 +13,24 @@ document.querySelector('#search-text').addEventListener('input', (e) => {
 })
 
 document.querySelector('#new-todo').addEventListener('submit', (e) => {
+    let text = e.target.elements.text.value.trim();
     e.preventDefault();
-    todos.push({
-        id: uuidv4(),
-        text: e.target.elements.text.value,
-        completed: false
-    });
-    saveTodos(todos)
-    renderTodos(todos, filters);
-    e.target.elements.text.value = '';
+
+    if (text.length > 0) {
+        todos.push({
+            id: uuidv4(),
+            text,
+            completed: false
+        });
+        saveTodos(todos)
+        renderTodos(todos, filters);
+        text = '';
+
+    }
+
+
+
+
 })
 
 document.querySelector('#hide-completed').addEventListener('change', (e) => {
