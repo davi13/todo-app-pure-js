d1e74e09d23d178a441b784b7ef4bcb2d6c64ebe
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-07-29 21:03:12 +0200

    moduling all the app part 1.0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
+import uuid from 'uuid/v4';
+import { renderTodos } from './views'
+renderTodos(todos, filters);
+
+document.querySelector('#search-text').addEventListener('input', (e) => {
+    filters.searchText = e.target.value;
+    renderTodos(todos, filters);
+})
+
+document.querySelector('#new-todo').addEventListener('submit', (e) => {
+    let text = e.target.elements.text.value.trim();
+    e.preventDefault();
+
+    if (text.length > 0) {
+        todos.push({
+            id: uuidv4(),
+            text,
+            completed: false
+        });
+        saveTodos(todos)
+        renderTodos(todos, filters);
+        e.target.elements.text.value = '';
+    }
+
+
+
+
+})
+
+document.querySelector('#hide-completed').addEventListener('change', (e) => {
+    filters.hideCompleted = e.target.checked;
+    renderTodos(todos, filters);
+})
+
+
 // Set up index.html to load the bundle
 // Make sure to load uuid via an npm module when necessary
 
--- src/todos.js
+++ src/todos.js
@@ -1,27 +1,42 @@
 // Setup the empty todos array
+let todos = [];
 
 // loadTodos
 // Arguments: none
 // Return value: none
+const loadTodos = () => {
 
+}
 // saveTodos
 // Arguments: none
 // Return value: none
+const saveTodos = () => {
 
+}
 // getTodos
 // Arguments: none
 // Return value: todos array
+const getTodos = () => {
 
+}
 // createTodo
 // Arguments: todo text
 // Return value: none
+const createTodo = (todo, text) => {
 
+}
 // removeTodo
 // Arguments: id of todo to remove
 // Return value: none
+const removeTodo = (todoId) => {
+
+}
 
 // toggleTodo
 // Arguments: id of todo to toggle
 // Return value: none
+const toggleTodo = (todoId) => {
 
+}
 // Make sure to call loadTodos and setup the exports
+export { loadTodos, saveTodos, getTodos, createTodo, removeTodo, toggleTodo };
