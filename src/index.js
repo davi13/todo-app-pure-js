bae4f12459e5f7a17565456949b114a2898536a5
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-07-30 11:59:00 +0200

    debugging

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import { renderTodos } from './views'
-import { setFilters, getFilters } from './filters'
-import { getTodos, createTodo, removeTodo, toggleTodo } from './todos';
+import { setFilters } from './filters'
+import { createTodo } from './todos';
 renderTodos();
 
 document.querySelector('#new-todo').addEventListener('submit', (e) => {
-
+    let text = e.target.elements.text.value.trim();
     e.preventDefault();
-    createTodo(e);
-    renderTodos();
+    if (text.length > 0) {
+        createTodo(text);
+        renderTodos();
+        e.target.elements.text.value = '';
+    }
+
 
 })
 
